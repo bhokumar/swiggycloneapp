7932389cb1b4f1566b5aab8f51398267b89aa7ec
bhokumar <bhoopendra.akgec@gmail.com>
2023-12-31 12:02:19 +0530

    Adding changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'tabs',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'tabs',
+    loadComponent: () => import('./pages/tabs/tabs.page').then( m => m.TabsPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/tabs/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('./pages/tabs/search/search.page').then( m => m.SearchPage)
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./pages/tabs/cart/cart.page').then( m => m.CartPage)
   },
 ];
